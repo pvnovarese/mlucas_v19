56e5bee6e570aabc13bd2a6992900d23e663f2a5
Paul V. Novarese <pvn@novarese.net>
2021-01-12 15:48:02 -0600

    Delete build_script_x86_64.sh

--- build_script_x86_64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#gcc -c -O3 -DUSE_SSE2 -DUSE_THREADS ../src/*.c
-#gcc -c -O3 -DUSE_AVX -mavx -DUSE_THREADS ../src/*.c
-gcc -c -O3 -DUSE_AVX2 -mavx2 -DUSE_THREADS ../src/*.c
-#gcc -c -O3 -DUSE_AVX512 -march=skylake-avx512 -DUSE_THREADS ../src/*.c
-#gcc -c -O3 -DUSE_AVX512 -march=knl -DUSE_THREADS ../src/*.c
-
-gcc -o Mlucas *.o -lm -lpthread -lrt
